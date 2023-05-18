008b66da7eefc1c39d12cba2bfe37bd800b29a4b
chapetta <cha.petta@hotmail.com>
2023-05-18 20:33:16 -0300

    Done: requisito 5 - implemente a funçao createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const obj = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return obj;
+};
+
+const estudante = createStudent('Leandrão, o Lobo Solitário');
+console.log(estudante.feedback());
 
 module.exports = createStudent;
